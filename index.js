46037b57cafaa6966e7b88fe3d7434f6ae7480c5
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-09-08 21:01:18 +0100

    adding input to pormpts for manager

--- index.js
+++ index.js
@@ -6,3 +6,64 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 const inquirer = require("inquirer");
+
+// control the manager prompt and what to build next.
+const managerPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the managers name?",
+        name: "managersName",
+      },
+      {
+        type: "input",
+        message: "What is the managers employee ID?",
+        name: "managersId",
+      },
+      {
+        type: "input",
+        message: "What is the managers email?",
+        name: "managersEmail",
+      },
+      {
+        type: "input",
+        message: "What is the managers office number?",
+        name: "managersOfficeNumber",
+      },
+    ])
+    .then((response) => {
+      mainMenu();
+    });
+};
+
+const mainMenu = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Who do you want to build next?",
+        name: "option",
+        choices: ["Engineer", "Intern", "Finish building team"],
+      },
+    ])
+    .then(() => {
+      switch (response.option) {
+        case "Finish building team":
+          console.log("finished!");
+          return;
+        case "Intern":
+          internPrompt();
+          break;
+        case "Engineer":
+          engineerPrompt();
+          break;
+      }
+    });
+};
+
+const main = () => {
+  managerPrompt();
+};
+
+main();
