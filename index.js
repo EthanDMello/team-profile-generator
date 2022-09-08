b2f3f19e1ecb90b9654e5bf56470eb41af7aa0f2
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-09-08 18:57:14 +0100

    structure of HTML added

---
+++ index.js
@@ -0,0 +1,8 @@
+// main code
+
+// import classes
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const inquirer = require("inquirer");
