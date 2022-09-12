ac7475199bc26b136ea5d65e439aa5f5edd45826
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-09-12 21:07:03 +0100

    added bulma styling to project and write filefunct

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -2,26 +2,31 @@ import fs from "fs";
 
 // generate HTML to publish
 export default function generateHTML(TeamMembers) {
-  TeamMembers.forEach((employee) => {
-    5;
-  });
+  TeamMembers.forEach((employee) => {});
   const newHTML = `<!DOCTYPE html>
   <html lang="en">
   <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teams Profile</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+    />
+    <link rel="stylesheet" href="./style.css" />
   </head>
 
   <body>
     <header>
-      <title>
-        <h1>My Team,/h1>
-      </title>
+    <h1>My Team</h1>
     </header>
   </body>
   </html>`;
 
-  fs.writeFile("index.html", newHTML);
+  fs.writeFile("./dist/index.html", newHTML, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 }
